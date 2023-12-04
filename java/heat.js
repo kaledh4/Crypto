e2c32d29ea1dc4048211bf613672e9f9ddc32758
Medo <kaled.h4@gmail.com>
2023-12-04 16:07:02 +0300

    Update heat.js

--- java/heat.js
+++ java/heat.js
@@ -1,24 +1,10 @@
-fetch('https://macrovar.com/united-states/us-dollar-index-dxy/')
+fetch('https://www.moneycontrol.com/markets/currencies/technical-analysis/dollar-index/daily/')
     .then(response => response.text())
     .then(html => {
         let parser = new DOMParser();
         let doc = parser.parseFromString(html, 'text/html');
-        let dxyPriceElement = doc.querySelector('#tab-11 > div > table > tbody > tr > td:nth-child(3) > span');
+        let dxyPriceElement = doc.querySelector('.pivoteLevels_web_mctable1___vjT0 > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(5)');
         let dxyPrice = dxyPriceElement.textContent;
         document.getElementById('dxyPrice').textContent = dxyPrice;
     })
     .catch(error => console.error('Error:', error));
-   
-const options = {
-    headers: {
-      'x-access-token': '$HEAT',
-    },
-};
-
-fetch('https://api.coinranking.com/v2/coin/bitcoin/history?timePeriod=5y', options)
-    .then((response) => response.json())
-    .then((result) => {
-        console.log(result);  // Add this line
-        let oldPrice = result.data.history[140].price;
-        console.log(`Bitcoin price 140 days ago: ${oldPrice}`);
-    });
